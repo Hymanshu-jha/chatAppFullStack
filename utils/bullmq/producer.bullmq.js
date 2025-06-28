9e3aad6ced2f66871a1ac29755b8d0e28dd775d8
Hymanshu-jha <himanshujhaa212@gmail.com>
2025-06-28 17:10:41 +0530

    redis implementation

--- utils/bullmq/producer.bullmq.js
+++ utils/bullmq/producer.bullmq.js
@@ -1,6 +1,6 @@
 import { Queue } from 'bullmq';
 import IORedis from 'ioredis';
-import { createClient } from 'redis';
+
 
 // -----------------------------
 // BULLMQ CONNECTION FIXED HERE
@@ -13,26 +13,7 @@ const connection = new IORedis({
   tls: {}, // required for Redis Cloud
 });
 
-// -----------------------------
-// TEST REDIS WITH createClient
-// -----------------------------
-const client = createClient({
-    username: 'default',
-    password: '94KbuOGipWXT3vYOsmVFEaNOBEEjXzv3',
-    socket: {
-        host: 'redis-16766.c90.us-east-1-3.ec2.redns.redis-cloud.com',
-        port: 16766
-    }
-});
 
-
-client.on('error', (err) => console.log('Redis Client Error', err));
-
-await client.connect();
-
-await client.set('foo', 'bar');
-const result = await client.get('foo');
-console.log(result); // bar
 
 // -----------------------------
 // BULLMQ QUEUE SETUP
